e9c1e24fcf551bdfae37f318f2caf6a3989c38c2
netsertzach <49474690+netsertzach@users.noreply.github.com>
2021-02-17 15:23:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="rtl">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>שכונת התקווה - לכל איש יש קול</title>
+    <link rel="stylesheet" href="css/foundation.min.css">
+    <link rel="stylesheet" href="css/app.css?v=1.0.3">
+</head>
+<body>
+<div class="reveal" id="exampleModal1" data-reveal>
+    <div class="text-center">
+        <h1>לכל איש יש קול</h1>
+        <p>ברוך הבא לאתר שכונת התקווה.</p>
+        <p>עבור עם העכבר על אחד מהסרטונים בכדי לשמוע מה יש לתושבים לומר. לחץ על הסרטון בכדי להגדילו למסך מלא.</p>
+        <p>לחץ על הסרטון לחיצה בודדת בכדי להגדילו. </p>
+        <p>לחיצה נוספת תוציא אותך ממסך מלא.</p>
+        <p>
+        <button class="close-modal button" >הבנתי!</button>
+        </p>
+    </div>
+    <button class="close-button" data-close aria-label="Close modal" type="button">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+<div class="grid-container full">
+    <div class="grid-x grid-padding-x">
+        <div class="small-12 cell text-center">
+            <h1 class="title">לכל איש יש קול - שכונת התקווה 2020</h1>
+        </div>
+    </div>
+</div>
+<div class="grid-container full">
+    <div class="grid-x grid-padding-x grid-margin-y">
+        <div class="cell small-3">
+            <div class="flex-video">
+                <h2 class="video-title">דוד בנימין חביב</h2>
+                <video src="videos/david-benyamin-haviv.mp4"
+                       poster="images/david-benyamin-haviv.jpg"
+                       class="video"
+                       muted
+                ></video>
+            </div>
+        </div>
+        <div class="cell small-3">
+            <div class="flex-video">
+                <h2 class="video-title">דוד הרואה</h2>
+                <video src="videos/david-haroeh.mp4"
+                       poster="images/david-haroeh.jpg"
+                       class="video"
+                       muted>
+                </video>
+            </div>
+        </div>
+        <div class="cell small-3">
+            <div class="flex-video">
+                <h2 class="video-title">דייזי משה</h2>
+                <video src="videos/dazy-moshe.mp4"
+                       poster="images/dazy-moshe.jpg"
+                       class="video"
+                       muted
+                ></video>
+            </div>
+        </div>
+        <div class="cell small-3">
+            <div class="flex-video">
+                <h2 class="video-title"> מזל ביבי </h2>
+                <video src="videos/mazal-bibi.mp4"
+                       poster="images/mazal-bibi.jpg"
+                       class="video"
+                       muted
+                ></video>
+            </div>
+        </div>
+        <div class="cell small-3">
+            <div class="flex-video">
+                <h2 class="video-title">שלום סינוואני</h2>
+                <video src="videos/shalom-sinvanni.mp4"
+                       poster="images/shalom-sinvanni.jpg"
+                       class="video"
+                       muted
+                ></video>
+            </div>
+        </div>
+        <div class="cell small-3">
+            <div class="flex-video">
+                <h2 class="video-title">מושיקו הלוי</h2>
+                <video src="videos/moshiko-halevi.mp4"
+                       poster="images/moshiko-halevi.jpg"
+                       class="video"
+                       muted
+                ></video>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script src="js/vendor/jquery.js"></script>
+<script src="js/vendor/what-input.js"></script>
+<script src="js/vendor/foundation.js"></script>
+<script src="js/app.js?v=1.0.3"></script>
+</body>
+</html>
